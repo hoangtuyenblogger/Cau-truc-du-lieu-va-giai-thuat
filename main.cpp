1191f34e15fea1ab7c8155f85391dfca05f782bd
hoangtuyenblogger <34962881+hoangtuyenblogger@users.noreply.github.com>
2019-09-20 08:43:40 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include"hoangtuyen.h"
+using namespace std;
+int main()
+{
+ 	phanso ps1, ps2, ps3;
+ 	ps1.nhap();
+ 	ps2.nhap();
+ 	ps3 = ps1 + ps2;
+ 	ps3.xuat();
+	return 0;
+}
